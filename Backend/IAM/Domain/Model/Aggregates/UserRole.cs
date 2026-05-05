f579af9a4965e6de55046d33c4a245a4c660ddfc
Aranda Vallejos, Oscar Gabriel <u202218167@upc.edu.pe>
2026-05-05 17:29:18 -0500

    fix: arreglar manejo de roles

--- Backend/IAM/Domain/Model/Aggregates/UserRole.cs
+++ Backend/IAM/Domain/Model/Aggregates/UserRole.cs
@@ -1,16 +1,20 @@
 using System.ComponentModel.DataAnnotations;
-using Backend.IAM.Domain.Model.ValueObjects;
 
 namespace Backend.IAM.Domain.Model.Aggregates;
 
-public class UserRole(ERole role)
+public class UserRole
 {
     public int Id { get; }
 
     public int UserId { get; private set; }
 
     [MaxLength(20)]
-    public string Role { get; private set; } = role.ToString();
+    public string Role { get; private set; } = string.Empty;
 
     public User User { get; private set; } = null!;
+
+    private UserRole() { }
+
+    public UserRole(Backend.IAM.Domain.Model.ValueObjects.ERole role)
+        => Role = role.ToString();
 }
