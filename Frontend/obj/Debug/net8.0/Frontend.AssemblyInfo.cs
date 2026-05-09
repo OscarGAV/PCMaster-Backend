0d69c63d8a920d42d3f91786cbf8cfd7d0b79cc3
Aranda Vallejos, Oscar Gabriel <u202218167@upc.edu.pe>
2026-05-09 17:47:37 -0500

    feat: remover footer innecesario

--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d87e6b3c776a439c218140687bad42a9a8228f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f2bca9b0758985d54f0c9cab36fc7f9dfc9068")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
